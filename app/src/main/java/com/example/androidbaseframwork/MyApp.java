e0206e51ac7fb90df6672b6751f9b4dd5065de80
luffy <>
2019-12-04 19:55:27 +0800

    gradle基本配置完成

---
+++ app/src/main/java/com/example/androidbaseframwork/MyApp.java
@@ -0,0 +1,16 @@
+package com.example.androidbaseframwork;
+
+import android.app.Application;
+
+/**
+ * @date 2019-12-04
+ * @Author luffy
+ * @description
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/example/androidbaseframwork/rxjava/RxbusActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidbaseframwork.rxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidbaseframwork.R;
+
+public class RxbusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxbus);
+    }
+}
